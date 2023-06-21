c81e5eb04bd0b1fc30bb99eb41d42847e066df9e
meixingc <realmeixing@gmail.com>
2023-06-20 21:08:22 -0400

    site registrations + settings auth model + urls

--- project/foodie/admin.py
+++ project/foodie/admin.py
@@ -1,1 +1,10 @@
 from django.contrib import admin
+from .models import User, BusinessProfile, Review, Follower, Post, Comment, Like
+
+admin.site.register(User)
+admin.site.register(BusinessProfile)
+admin.site.register(Review)
+admin.site.register(Follower)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
